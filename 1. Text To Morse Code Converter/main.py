f22783443e9a8ba7cbb265a1792b52bdfb19f4d4
Tong Lin <wowawawoa@gmail.com>
2023-10-09 20:32:44 +1030

    init project

---
+++ 1. Text To Morse Code Converter/main.py
@@ -0,0 +1,26 @@
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.',
+}
+
+
+def text_to_morse(text):
+    morse_code = []
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+        else:
+            morse_code.append('?')  # Use '?' for unknown characters
+    return ' '.join(morse_code)
+
+
+input_text = input("Input to convert to Morse Code: ")
+morse_code = text_to_morse(input_text)
+print(f"Morse Code: {morse_code}")
